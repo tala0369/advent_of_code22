b48ab219a07e4e2c994a21bebee2a4539abc9567
tala0369 <talin.larson@gmail.com>
2022-12-31 16:37:56 -0700

    And pt2

--- 1/calorie_checker.py
+++ 1/calorie_checker.py
@@ -13,18 +13,33 @@ def get_input_data():
     ]
     return lines
 
-def get_max_calories(food_list):
-    max_calorie_count = 0
+def get_calorie_count(food_list):
+    elves = []
     for i in range(0, len(food_list)):
-        total_cals = sum(food_list[i])
-        if total_cals > max_calorie_count:
-            max_calorie_count = total_cals
-            max_elf = i
-    print(f"Elf {max_elf} @ {max_calorie_count}")
+        elf = {
+            "num": i,
+            "snack_list": food_list[i],
+            "total_calorie_count": sum(food_list[i])
+        }
+        elves.append(elf)
+    elves_sorted = sorted(elves, key=lambda d: d['total_calorie_count'], reverse=True)
+    return elves_sorted
+
+def get_max_calories(calorie_ref, num):
+    total_cals = 0
+    for i in range(0,num):
+        elf = calorie_ref[i]
+        this_elf_num = elf["num"]
+        this_elf_cals = elf["total_calorie_count"]
+        print(f"Elf {this_elf_num} @ {this_elf_cals}")
+        total_cals += this_elf_cals
+    print(f"Total Cals: {total_cals}")
         
         
 
 if __name__ == "__main__":
     food_list = get_input_data()
-    get_max_calories(food_list)
+    calorie_ref = get_calorie_count(food_list)
+    get_max_calories(calorie_ref, 1)
+    get_max_calories(calorie_ref, 3)
     print("Done")
